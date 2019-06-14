3af4f934e9f2cc5cc0efd7d79ca7ebb9d8f303ac
Wandji69 <collinschuwwa@gmail.com>
2019-06-15 00:23:18 +0100

    updating dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ var getConfig = function () {
 	  } catch (err) {
 	    // create file with defaults if not found
 	    config = {
-	      'LOCAL_OWA_FOLDER': '/home/collins/.OpenMRSowa/',
+	      'LOCAL_OWA_FOLDER': '/home/collins/.OpenMRSowa/owa/',
 	      'APP_ENTRY_POINT': 'http://localhost:8080/openmrs/owa/commonlabtest/index.html'
 	    };
 
